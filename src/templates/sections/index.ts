fd843358c82357af4cd7649cc55c828c692d665b
AlexBar <nesferat@mail.ru>
2025-02-26 18:04:34 +0300

    add section-slider-2

--- src/templates/sections/index.ts
+++ src/templates/sections/index.ts
@@ -2,8 +2,10 @@ import "./section-cards-list"
 import "./section-categories"
 import { initSliderFull } from "./section-slider-full";
 import { initCardsSlider } from "./section-cards-slider";
+import { initSlider2Elems } from "./section-slider-2";
 
 export const initSections = () => {
   initSliderFull()
   initCardsSlider()
+  initSlider2Elems()
 }
--- src/templates/sections/section-slider-2/index.ts
+++ src/templates/sections/section-slider-2/index.ts
@@ -1,22 +1,22 @@
 import "./index.scss"
 import { ESwiperElems } from "@js/libs/swiper";
-import { Nullable } from "@js/types";
 import { Swiper } from "swiper";
-import { Navigation } from "swiper/modules";
+import { Nullable } from "@js/types";
 import { mqData } from "@js/helpers/media.ts";
+import { Navigation } from "swiper/modules";
 
-export const initCardsSlider = () => {
+export const initSlider2Elems = () => {
   const { breakpoints } = mqData()
-  const sections = document.querySelectorAll( '.section-cards-slider' )
   const breakpointsNum = breakpoints.toNumbers()
+  const sections = document.querySelectorAll( '.section-slider-2' )
 
   sections.forEach( section => {
-    const sliderElem: Nullable<HTMLElement> = section.querySelector( ESwiperElems.Swiper )
+    const swiperElem: Nullable<HTMLElement> = section.querySelector( ESwiperElems.Swiper )
     const prevButton: Nullable<HTMLElement> = section.querySelector( '.slider-button-prev' )
     const nextButton: Nullable<HTMLElement> = section.querySelector( '.slider-button-next' )
 
-    if ( sliderElem ) {
-      new Swiper( sliderElem, {
+    if ( swiperElem ) {
+      new Swiper( swiperElem, {
         modules: [ Navigation ],
         slidesPerView: 2.1,
         spaceBetween: 8,
@@ -26,11 +26,8 @@ export const initCardsSlider = () => {
         },
         breakpoints: {
           [ breakpointsNum.md ]: {
-            spaceBetween: 16,
-            slidesPerView: 3,
-          },
-          [ breakpointsNum.lg ]: {
-            slidesPerView: 4,
+            slidesPerView: 2,
+            spaceBetween: 38,
           }
         }
       } )
