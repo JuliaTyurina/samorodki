bb426a8ff3c2c2b51f926f3e7c58f1222d090cfb
AlexBar <nesferat@mail.ru>
2025-02-26 15:34:59 +0300

    add section-cards-slider

--- src/templates/sections/index.ts
+++ src/templates/sections/index.ts
@@ -1,7 +1,9 @@
 import "./section-cards-list"
 import "./section-categories"
 import { initSliderFull } from "./section-slider-full";
+import { initCardsSlider } from "./section-cards-slider";
 
 export const initSections = () => {
   initSliderFull()
+  initCardsSlider()
 }
---
+++ src/templates/sections/section-cards-slider/index.ts
@@ -0,0 +1,41 @@
+import "./index.scss"
+import { ESwiperElems } from "@js/libs/swiper";
+import { Nullable } from "@js/types";
+import { Swiper } from "swiper";
+import { Navigation } from "swiper/modules";
+import { mqData } from "@js/helpers/media.ts";
+
+export const initCardsSlider = () => {
+  const { breakpoints } = mqData()
+  const sections = document.querySelectorAll( '.section-cards-slider' )
+  const breakpointsNum = breakpoints.toNumbers()
+
+  sections.forEach( section => {
+    const sliderElem: Nullable<HTMLElement> = section.querySelector( ESwiperElems.Swiper )
+    const prevButton: Nullable<HTMLElement> = section.querySelector( '.slider-button-prev' )
+    const nextButton: Nullable<HTMLElement> = section.querySelector( '.slider-button-next' )
+
+    console.log( breakpoints.toNumbers() )
+
+    if ( sliderElem ) {
+      new Swiper( sliderElem, {
+        modules: [ Navigation ],
+        slidesPerView: 2.1,
+        spaceBetween: 8,
+        navigation: {
+          prevEl: prevButton,
+          nextEl: nextButton
+        },
+        breakpoints: {
+          [ breakpointsNum.md ]: {
+            spaceBetween: 16,
+            slidesPerView: 3,
+          },
+          [ breakpointsNum.lg ]: {
+            slidesPerView: 4,
+          }
+        }
+      } )
+    }
+  } )
+}
